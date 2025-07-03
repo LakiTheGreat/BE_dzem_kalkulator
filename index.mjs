4a1e69b395386b3a51d153541a71407c7593e883
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-03 17:15:47 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -6,18 +6,30 @@ import lookupRouter from './src/routes/lookups.mjs';
 
 dotenv.config();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://dzem-kalkulator.vercel.app',
+];
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    // credentials: true, // Only include this if you use cookies or Authorization headers
+  })
+);
 
 app.use(express.json());
 app.use(lookupRouter);
-
-app.use(
-  cors({
-    origin: '*',
-  })
-);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

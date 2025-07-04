91ccbf0f07820b2db44f090e104c36f3332389bb
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-04 09:50:10 +0200

    logger

--- src/routes/lookups.ts
+++ src/routes/lookups.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import { fruits } from '../mockData/index.js';
+import logger from '../utils/logger.js';
 
 const router = Router();
 
 router.get('/fruits', (req, res) => {
+  console.log('GET /fruits - POZVANO');
+  logger.info('GET /fruits - POZVANO - WINSTON LOGER');
   res.status(200).json(fruits);
 });
 

c7a511b50426199beb16e52380d500a62d76986b
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-10 17:39:17 +0200

    schema

--- dist/validationSchemas/fruitSchema.js
+++ dist/validationSchemas/fruitSchema.js
@@ -2,8 +2,8 @@ export const fruitSchema = {
     label: {
         notEmpty: { errorMessage: 'Field "value" is required' },
         isLength: {
-            options: { min: 1, max: 50 },
-            errorMessage: 'Field "value" must be between 1 and 50 characters',
+            options: { min: 3, max: 50 },
+            errorMessage: 'Field "value" must be between 3 and 50 characters',
         },
     },
 };

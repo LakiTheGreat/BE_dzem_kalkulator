e80ddb5086160d995cd8482ed260425ab46f5ef8
Nikola Lakobrija <nikola.lakobrija@enlightit.rs>
2025-07-10 17:49:43 +0200

    removed next from handleVadliatioonErrors

--- dist/utils/handleValidationErrors.js
+++ dist/utils/handleValidationErrors.js
@@ -3,6 +3,7 @@ export function handleValidationErrors(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
+        return;
     }
     next();
 }
--- src/utils/handleValidationErrors.ts
+++ src/utils/handleValidationErrors.ts
@@ -9,6 +9,7 @@ export function handleValidationErrors(
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
+    return;
   }
   next();
 }
